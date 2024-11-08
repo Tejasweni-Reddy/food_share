2ca30985b693e0b2def4d4dda8ef8c401a27202d
teju <teju789reddy@gmail.com>
2024-11-08 16:05:49 +0530

    created the new project food_recipe_share

---
+++ src/main/java/com/sharing/food_recipe_share/FoodRecipeShareApplication.java
@@ -0,0 +1,13 @@
+package com.sharing.food_recipe_share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipeShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecipeShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharing/food_recipe_share/FoodRecipeShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharing.food_recipe_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipeShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
